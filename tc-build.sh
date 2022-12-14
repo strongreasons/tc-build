9668d2821d2a90801476f64e5127bac5d2cfa67e
GhostMaster69-dev <rathore6375@gmail.com>
2022-12-15 00:52:31 +0530

    tc-build: Add X86 architecture to llvm target list
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- tc-build.sh
+++ tc-build.sh
@@ -53,7 +53,7 @@ CC=clang CXX=clang++ ./build-llvm.py \
 	--clang-vendor "$LLVM_NAME" \
 	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3" \
 	--projects "clang;lld;polly" \
-	--targets "ARM;AArch64" \
+	--targets "ARM;AArch64;X86" \
 	--shallow-clone \
 	--build-type "Release" 2>&1 | tee build.log
 
