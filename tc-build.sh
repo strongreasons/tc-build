09fcd7b353fdbb3b482cb40a82ad8a87714ad69c
GhostMaster69-dev <rathore6375@gmail.com>
2022-12-18 01:42:46 +0530

    tc-build: Re-write script to pass sfmt and shellcheck tests
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- tc-build.sh
+++ tc-build.sh
@@ -24,22 +24,23 @@ DIR="$(pwd ...)"
 # Inlined function to post a message
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 tg_post_msg() {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
+    curl -s -X POST "$BOT_MSG_URL" \
+        -d chat_id="$TG_CHAT_ID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="$1"
 
 }
 tg_post_build() {
-	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
-	-F chat_id="$TG_CHAT_ID"  \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$3"
+    curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
+        -F chat_id="$TG_CHAT_ID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="$3"
 }
 
 # Build Info
-rel_date="$(date "+%Y%m%d")" # ISO 8601 format
+rel_date="$(date "+%Y%m%d")"              # ISO 8601 format
 rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
@@ -50,19 +51,21 @@ tg_post_msg "<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><c
 msg "$LLVM_NAME: Building LLVM..."
 tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
 CC=clang CXX=clang++ ./build-llvm.py \
-	--clang-vendor "$LLVM_NAME" \
-	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3" \
-	--projects "clang;lld;polly" \
-	--targets "ARM;AArch64;X86" \
-	--shallow-clone \
-	--build-type "Release" 2>&1 | tee build.log
+    --clang-vendor "$LLVM_NAME" \
+    --defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3" \
+    --projects "clang;lld;polly" \
+    --targets "ARM;AArch64;X86" \
+    --shallow-clone \
+    --build-type "Release" 2>&1 | tee build.log
 
 # Check if the final clang binary exists or not.
-[ ! -f install/bin/clang-1* ] && {
-	err "Building LLVM failed ! Kindly check errors !!"
-	tg_post_build "build.log" "$TG_CHAT_ID" "Error Log"
-	exit 1
-}
+for clang in install/bin/clang-1*; do
+    if [ ! -f "$clang" ]; then
+        err "Building LLVM failed ! Kindly check errors !!"
+        tg_post_build "build.log" "$TG_CHAT_ID" "Error Log"
+        exit 1
+    fi
+done
 
 # Build binutils
 msg "$LLVM_NAME: Building binutils..."
@@ -75,34 +78,34 @@ rm -f install/lib/*.a install/lib/*.la
 
 # Strip remaining products
 for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
-	strip -s "${f: : -1}"
+    strip -s "${f::-1}"
 done
 
 # Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
 for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
-	# Remove last character from file output (':')
-	bin="${bin: : -1}"
+    # Remove last character from file output (':')
+    bin="${bin::-1}"
 
-	echo "$bin"
-	patchelf --set-rpath "$DIR/install/lib" "$bin"
+    echo "$bin"
+    patchelf --set-rpath "$DIR/install/lib" "$bin"
 done
 
 # Release Info
 pushd llvm-project || exit
 llvm_commit="$(git rev-parse HEAD)"
-short_llvm_commit="$(cut -c-8 <<< "$llvm_commit")"
+short_llvm_commit="$(cut -c-8 <<<"$llvm_commit")"
 popd || exit
 
 llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
-binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
+binutils_ver="$(echo binutils-* | sed "s/binutils-//g")"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
 tg_post_msg "<b>$LLVM_NAME: Toolchain compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
 
 # Push to GitHub
 # Update Git repository
-git config --global user.name $GH_USERNAME
-git config --global user.email $GH_EMAIL
+git config --global user.name "$GH_USERNAME"
+git config --global user.email "$GH_EMAIL"
 git clone "https://$GH_USERNAME:$GH_TOKEN@$GH_PUSH_REPO_URL" rel_repo
 pushd rel_repo || exit
 rm -fr ./*
