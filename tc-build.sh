5b03e510ca3b91181be4d84bb424317a461d49e3
strongreasons <abenkenari@gmail.com>
2024-06-12 23:39:04 +0700

    Update tc-build.sh

--- tc-build.sh
+++ tc-build.sh
@@ -1,100 +1,83 @@
 #!/usr/bin/env bash
 
-# Secret Variable for CI
-# LLVM_NAME | Your desired Toolchain Name
-# TG_TOKEN | Your Telegram Bot Token
-# TG_CHAT_ID | Your Telegram Channel / Group Chat ID
-# GH_USERNAME | Your Github Username
-# GH_EMAIL | Your Github Email
-# GH_TOKEN | Your Github Token ( repo & repo_hook )
-# GH_PUSH_REPO_URL | Your Repository for store compiled Toolchain ( without https:// or www. ) ex. github.com/xyz-prjkt/xRageTC.git
-
 # Function to show an informational message
 msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
-
 err() {
-    echo -e "\e[1;41m$*\e[0m"
+    echo -e "\e[1;41$*\e[0m"
 }
 
-# Set a directory
-DIR="$(pwd ...)"
+# Environment Config
+export TELEGRAM_TOKEN=6410284454:AAFHrE_XZtikh0v8L7IoDVr1RAMuno3LjeI
+export TELEGRAM_CHAT=-1002088104319
+export GH_TOKEN=$GH_TOKEN
+export BRANCH=main
+export CACHE=1
 
-# Inlined function to post a message
-export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
-tg_post_msg() {
-    curl -s -X POST "$BOT_MSG_URL" \
-        -d chat_id="$TG_CHAT_ID" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="$1"
+# Get home directory
+HOME_DIR="$(pwd)"
 
+# Telegram Setup
+git clone --depth=1 https://github.com/elynord/Telegram Telegram
+
+TELEGRAM="$HOME_DIR/Telegram/telegram"
+send_msg() {
+    "${TELEGRAM}" -H -D \
+        "$(
+            for POST in "${@}"; do
+                echo "${POST}"
+            done
+        )"
 }
 
-tg_post_build() {
-    curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
-        -F chat_id="$TG_CHAT_ID" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="$3"
+send_file() {
+    "${TELEGRAM}" -H \
+        -f "$1" \
+        "$2"
 }
 
-tg_post_erlog() {
-    curl -F document=@"build.log" "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
-        -F chat_id="$TG_CHAT_ID" \
-        -F caption="Build ran into errors, plox check logs"
-}
-
-# Build Info
-rel_date="$(date "+%Y%m%d")"              # ISO 8601 format
-rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
-builder_commit="$(git rev-parse HEAD)"
-
-# Send a notificaton to TG
-tg_post_msg "<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
-
-# Build LLVM
-msg "$LLVM_NAME: Building LLVM..."
-tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
-CC=clang CXX=clang++ CFLAGS=-O3 CXXFLAGS=-O3 ./build-llvm.py \
-    --clang-vendor "$LLVM_NAME" \
-    --defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3" \
-    --projects "all" \
-    --targets "ARM;AArch64;X86" \
-    --shallow-clone \
+# Building LLVM's
+source "$HOME_DIR"/.bashrc && source "$HOME_DIR"/.profile
+if [[ $CACHE -eq 1 ]]; then
+    ccache -M 256G
+    export USE_CCACHE=1
+fi
+msg "Building LLVM's ..."
+send_msg "<b>Start build StRess Clang from <code>[ $BRANCH ]</code> branch</b>"
+./build-llvm.py \
+    --defines LLVM_PARALLEL_COMPILE_JOBS="$(nproc)" LLVM_PARALLEL_LINK_JOBS="$(nproc)" CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 \
+    --install-folder "$HOME_DIR/install" \
+    --no-update \
+    --no-ccache \
     --quiet-cmake \
-    --build-type "Release" \
-    --branch "release/17.x" 2>&1 | tee build.log
+    --ref "$BRANCH" \
+    --shallow-clone \
+    --targets AArch64 ARM X86 \
+    --vendor-string "StRess"
 
-# Check if the final clang binary exists or not.
-for clang in install/bin/clang-1*; do
-    if [ ! -f "$clang" ]; then
-        err "Building LLVM failed ! Kindly check errors !!"
-        tg_post_erlog
-        exit 1
+# Check if the final clang binary exists or not
+for file in install/bin/clang-1*; do
+    if [ -e "$file" ]; then
+        msg "LLVM's build successful"
+    else
+        err "LLVM's build failed!"
+        send_msg "LLVM's build failed!"
+        exit
     fi
 done
 
 # Build binutils
-msg "$LLVM_NAME: Building binutils..."
-tg_post_msg "<b>$LLVM_NAME: Building Binutils. . .</b>"
-CC=gcc CXX=g++ CFLAGS=-O3 CXXFLAGS=-O3 ./build-binutils.py --targets arm aarch64 x86_64
-
-# Check if the binutils dir exists or not
-for binutils in binutils-*; do
-    if [ ! -d "$binutils" ]; then
-        err "curl: failed to download binutils from https://ftp.gnu.org/gnu/binutils"
-        tg_post_erlog
-        exit 1
-    fi
-done
+msg "Build binutils ..."
+./build-binutils.py \
+    --install-folder "$HOME_DIR/install" \
+    --targets arm aarch64 x86_64
 
 # Remove unused products
 rm -fr install/include
 rm -f install/lib/*.a install/lib/*.la
 
-# Strip remaining products
+# Strips remaining products
 for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
     strip -s "${f::-1}"
 done
@@ -105,36 +88,122 @@ for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | gre
     bin="${bin::-1}"
 
     echo "$bin"
-    patchelf --set-rpath "$DIR/install/lib" "$bin"
+    patchelf --set-rpath "$DIR/../lib" "$bin"
 done
 
-# Release Info
-pushd llvm-project || exit
+# Git config
+git config --global user.name "strongreasons"
+git config --global user.email "strongreasons@users.noreply.github.com"
+
+# Get Clang Info
+pushd "$HOME_DIR"/src/llvm-project || exit
 llvm_commit="$(git rev-parse HEAD)"
 short_llvm_commit="$(cut -c-8 <<<"$llvm_commit")"
 popd || exit
-
 llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
-binutils_ver="$(echo binutils-* | sed "s/binutils-//g")"
-clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
+clang_version="$("$HOME_DIR"/install/bin/clang --version | head -n1 | cut -d' ' -f4)"
+build_date="$(TZ=Asia/Jakarta date +"%Y-%m-%d")"
+tags="StRess-Clang-$clang_version-release"
+file="StRess-Clang-$clang_version.tar.gz"
+clang_link="https://github.com/strongreasons/stress-clang/releases/download/$tags/$file"
 
-tg_post_msg "<b>$LLVM_NAME: Toolchain compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
+# Get binutils version
+binutils_version=$(grep "LATEST_BINUTILS_RELEASE" build-binutils.py)
+binutils_version=$(echo "$binutils_version" | grep -oP '\(\s*\K\d+,\s*\d+,\s*\d+' | tr -d ' ')
+binutils_version=$(echo "$binutils_version" | tr ',' '.')
 
-# Push to GitHub
-# Update Git repository
-git config --global user.name "$GH_USERNAME"
-git config --global user.email "$GH_EMAIL"
-git clone "https://$GH_USERNAME:$GH_TOKEN@$GH_PUSH_REPO_URL" rel_repo
-pushd rel_repo || exit
-rm -fr ./*
-cp -r ../install/* .
-git checkout README.md LICENSE # keep this as it's not part of the toolchain itself
-git add .
-git commit -asm "$LLVM_NAME: Bump to $rel_date release
-
-LLVM commit: $llvm_commit_url
-Clang Version: $clang_version
-Binutils version: $binutils_ver"
-git push -f
+# Create simple info
+pushd "$HOME_DIR"/install || exit
+{
+    echo "# Quick Info
+* Build Date : $build_date
+* Clang Version : $clang_version
+* Binutils Version : $binutils_version
+* Compiled Based : $llvm_commit_url"
+} >>README.md
+tar -czvf ../"$file" .
 popd || exit
-tg_post_msg "<b>$LLVM_NAME: Toolchain pushed to <code>https://$GH_PUSH_REPO_URL</code></b>"
+
+# Push
+git clone "https://strongreasons:$GIT_TOKEN@github.com/strongreasons/stress-clang.git" rel_repo
+pushd rel_repo || exit
+if [ -d "$BRANCH" ]; then
+    echo "$clang_link" >"$BRANCH"/link.txt
+    cp -r "$HOME_DIR"/install/README.md "$BRANCH"
+else
+    mkdir -p "$BRANCH"
+    echo "$clang_link" >"$BRANCH"/link.txt
+    cp -r "$HOME_DIR"/install/README.md "$BRANCH"
+fi
+git add .
+git commit -asm "StRess-Clang-$clang_version: $(TZ=Asia/Jakarta date +"%Y%m%d")"
+git push -f origin main
+
+# Check tags already exists or not
+overwrite=y
+git tag -l | grep "$tags" || overwrite=n
+popd || exit
+
+# Upload to github release
+failed=n
+if [ "$overwrite" == "y" ]; then
+    ./github-release edit \
+        --security-token "$GIT_TOKEN" \
+        --user "StRess-Stuff" \
+        --repo "StRess-Clang" \
+        --tag "$tags" \
+        --description "$(cat "$HOME_DIR"/install/README.md)"
+
+    ./github-release upload \
+        --security-token "$GIT_TOKEN" \
+        --user "StRess-Stuff" \
+        --repo "StRess-Clang" \
+        --tag "$tags" \
+        --name "$file" \
+        --file "$HOME_DIR/$file" \
+        --replace || failed=y
+else
+    ./github-release release \
+        --security-token "$GIT_TOKEN" \
+        --user "StRess-Stuff" \
+        --repo "StRess-Clang" \
+        --tag "$tags" \
+        --description "$(cat "$HOME_DIR"/install/README.md)"
+
+    ./github-release upload \
+        --security-token "$GIT_TOKEN" \
+        --user "StRess-Stuff" \
+        --repo "StRess-Clang" \
+        --tag "$tags" \
+        --name "$file" \
+        --file "$HOME_DIR/$file" || failed=y
+fi
+
+# Handle uploader if upload failed
+while [ "$failed" == "y" ]; do
+    failed=n
+    msg "Upload again"
+    ./github-release upload \
+        --security-token "$GIT_TOKEN" \
+        --user "StRess-Stuff" \
+        --repo "StRess-Clang" \
+        --tag "$tags" \
+        --name "$file" \
+        --file "$HOME_DIR/$file" \
+        --replace || failed=y
+done
+
+# Send message to telegram
+send_msg "
+<b>----------------- Quick Info -----------------</b>
+<b>Build Date : </b>
+* <code>$build_date</code>
+<b>Clang Version : </b>
+* <code>$clang_version</code>
+<b>Binutils Version : </b>
+* <code>$binutils_version</code>
+<b>Compile Based : </b>
+* <a href='$llvm_commit_url'>$llvm_commit_url</a>
+<b>Push Repository : </b>
+* <a href='https://github.com/strongreasons/stress-clang.git'>StRess-Clang</a>
+<b>-------------------------------------------------</b>"
